a980392edccef0f82584448aac37c29674a180a1
christopher-caldwell <well.done.514@gmail.com>
2021-06-01 10:30:04 -0600

    formatting

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 /** Shuffles the order of a given array */
 export const shuffle = <ArrayItem>(originalSet: ArrayItem[]): ArrayItem[] => {
-	const maxIndex = originalSet.length - 1
-	for(let iterator = 0; iterator < maxIndex; iterator++) {
-		const randomIndex = Math.floor(Math.random() * maxIndex)
-		const currentItem = originalSet[iterator]
+  const maxIndex = originalSet.length - 1
+  for (let iterator = 0; iterator < maxIndex; iterator++) {
+    const randomIndex = Math.floor(Math.random() * maxIndex)
+    const currentItem = originalSet[iterator]
     originalSet[iterator] = originalSet[randomIndex]
     originalSet[randomIndex] = currentItem
-	}
-	return originalSet
+  }
+  return originalSet
 }
