4e3bddb5d9d5546add59fba9da08d4894f42a2b6
christopher-caldwell <well.done.514@gmail.com>
2021-06-06 17:42:35 -0600

    correcting tests for ensuring uniqueness

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 /** Shuffles the order of a given array */
-export const shuffle = <ArrayItem>(originalSet: ArrayItem[]): ArrayItem[] => {
-  const maxIndex = originalSet.length - 1
+export const shuffle = <ArrayItem>(
+  originalSet: ArrayItem[],
+  { shouldEnsureUnique }: ShuffleOptions = {}
+): ArrayItem[] => {
+  const mutableCopy = [...originalSet]
+  const maxIndex = mutableCopy.length - 1
   for (let iterator = 0; iterator < maxIndex; iterator++) {
     const randomIndex = Math.floor(Math.random() * maxIndex)
-    const currentItem = originalSet[iterator]
-    originalSet[iterator] = originalSet[randomIndex]
-    originalSet[randomIndex] = currentItem
+    const currentItem = mutableCopy[iterator]
+    mutableCopy[iterator] = mutableCopy[randomIndex]
+    mutableCopy[randomIndex] = currentItem
   }
-  return originalSet
+  if (shouldEnsureUnique) {
+    if (JSON.stringify(originalSet) !== JSON.stringify(mutableCopy)) return mutableCopy
+    return shuffle(originalSet, { shouldEnsureUnique })
+  }
+  return mutableCopy
+}
+
+export interface ShuffleOptions {
+  /** If present, the returned result will not match the original set. The chances are low, but the original set could be returned due to RNG. */
+  shouldEnsureUnique?: boolean
 }
--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,16 +1,23 @@
 import { shuffle } from '@/index'
 
-const baseNumberArray = [1,2,3]
-const baseStringArray = ['1','2','3']
+const baseNumberArray = [1, 2, 3]
+const baseStringArray = ['1', '2', '3']
 const baseObjectArray = [{ one: '1' }, { one: '2' }, { one: '3' }]
 
 describe('Shuffling number array', () => {
   it('The original arrays should be the same', () => {
     expect(baseNumberArray).toStrictEqual(baseNumberArray)
   })
-  it('Shuffled arrays should be different', () => {
-    const shuffled = shuffle([...baseNumberArray])
+  it('Shuffled arrays should be different when passed unique flag', () => {
+    const shuffled = shuffle([...baseNumberArray], { shouldEnsureUnique: true })
     expect(baseNumberArray).not.toStrictEqual(shuffled)
+  })
+  // Cannot test uniqueness here as there is potential for the shuffled set to be the same
+  it('Shuffled arrays should not error out', () => {
+    const main = (): void => {
+      shuffle([...baseNumberArray])
+    }
+    expect(main).not.toThrowError()
   })
 })
 
@@ -18,9 +25,16 @@ describe('Shuffling string array', () => {
   it('The original arrays should be the same', () => {
     expect(baseStringArray).toStrictEqual(baseStringArray)
   })
-  it('Shuffled arrays should be different', () => {
-    const shuffled = shuffle([...baseStringArray])
+  it('Shuffled arrays should be different when passed unique flag', () => {
+    const shuffled = shuffle([...baseStringArray], { shouldEnsureUnique: true })
     expect(baseStringArray).not.toStrictEqual(shuffled)
+  })
+  // Cannot test uniqueness here as there is potential for the shuffled set to be the same
+  it('Shuffled arrays should not error out', () => {
+    const main = (): void => {
+      shuffle([...baseStringArray])
+    }
+    expect(main).not.toThrowError()
   })
 })
 
@@ -28,8 +42,15 @@ describe('Shuffling object arrays', () => {
   it('The original arrays should be the same', () => {
     expect(baseObjectArray).toStrictEqual(baseObjectArray)
   })
-  it('Shuffled arrays should be different', () => {
-    const shuffled = shuffle([...baseStringArray])
+  it('Shuffled arrays should be different when passed unique flag', () => {
+    const shuffled = shuffle([...baseStringArray], { shouldEnsureUnique: true })
     expect(baseObjectArray).not.toStrictEqual(shuffled)
+  })
+  // Cannot test uniqueness here as there is potential for the shuffled set to be the same
+  it('Shuffled arrays should not error out', () => {
+    const main = (): void => {
+      shuffle([...baseObjectArray])
+    }
+    expect(main).not.toThrowError()
   })
 })
